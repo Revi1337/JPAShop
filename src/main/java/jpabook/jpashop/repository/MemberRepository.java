79a937fb6cede4e6f01c4fe68c5e26a823aea922
Revi1337 <david122123@gmail.com>
2023-02-20 23:53:02 +0900

    Create MemberRepository

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,21 +3,38 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
+@Repository @RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext // 스프링컨테이너 위에서 동작하기 때문에  @PersistenceContext 가 있으면 EntityManger 를 주입해줌. (따로 EntityMangerFactory 를 통해 Manger 를 만들 필요가 없음.)
-    private EntityManager em;
+//    @PersistenceUnit    // @PersistenceUnit 가 있으면 스프링이 만든 EntityMangerFactory 빈을 주입해줌, (없어도 됨.)
+//    private EntityManagerFactory emf;
 
-        public Long save(Member member) {
+//    @PersistenceContext // 1. @PersistenceContext 가 있으면 스프링이 만든 EntityManger 빈을 주입해줌. (따로 EntityMangerFactory 를 통해 Manger 를 만들 필요가 없음.)
+//    private EntityManager em;
+
+    private final EntityManager em; // 2. @PersistenceContext 말고 생성자 주입으로도 가능.
+
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member as m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member as m where m.name=:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 
 }
