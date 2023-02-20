d43488a92b43cd293d030ec72fac4bd4a7b9c031
Revi1337 <david122123@gmail.com>
2023-02-20 23:53:16 +0900

    Create MemberService

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service @RequiredArgsConstructor @Transactional(readOnly = true) // 1. @Transactional 를 클래스 단위에 설정하면 public 이나 protected 메서드가 걸려들어감. (readonly 라 변경감지 등 체클르하지 않아 성능에 유리)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional  // 2. @Transactional 메서드 단위에 설정하면 우선권을 갖음.(여기는 readonly=true 가 아니기 떄문에 변경감지 등을 수행.)
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty())
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
