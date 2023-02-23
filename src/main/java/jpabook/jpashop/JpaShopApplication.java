b996c31a6a9fc6b187bb1507d1fa3739e25b6ac3
Revi1337 <david122123@gmail.com>
2023-02-24 00:50:19 +0900

    MemberSearch API

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -1,13 +1,44 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaShopApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(MemberService memberService) {
+        return args -> {
+            Member member3 = new Member();
+            member3.setName("test1");
+            member3.setAddress(new Address("twest", "asdf", "2134"));
+            memberService.join(member3);
+
+            Member member4 = new Member();
+            member4.setName("test2");
+            member4.setAddress(new Address("wqwer213", "reqr", "asdf"));
+            memberService.join(member4);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setAddress(new Address("서울", "test", "11111"));
+            memberService.join(member);
+
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setAddress(new Address("부산", "qwer", "12322"));
+            memberService.join(member2);
+        };
     }
 
 }
--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,9 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController @RequiredArgsConstructor
 public class MemberApiController {
 
@@ -43,12 +46,54 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 회원 수정 API updateMemberV2 은 회원 정보를 부분 업데이트 한다. 여기서 PUT 방식을
+     * 사용했는데, PUT은 전체 업데이트를 할 때 사용하는 것이 맞다. 부분 업데이트를 하려면 PATCH를
+     * 사용하거나 POST를 사용하는 것이 REST 스타일에 맞다
+     */
     @PutMapping(value = "/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable(name = "id") Long id,
                                                @RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스
+     생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api 가 이거하나인가! 화면에 종속적이지 마라
+    @GetMapping(value = "/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     * 엔티티를 DTO로 변환해서 반환한다.
+     * 엔티티가 변해도 API 스펙이 변경되지 않는다.
+     * 추가로 Result 클래스로 컬렉션을 감싸서 향후 필요한 필드를 추가할 수 있다.
+     * 바로 List 를 내보내면 json 형태가깨짐 --> 배열안에 JSON 이 나감 --> [{}, {}] --> 유연성 저하
+     * 하지만, Result 클래스로 컬렉션을 감싸서 리턴하면 --> JSON 안에 배열이 나감 --> {[]} --> 유연성 상승
+     */
+    @GetMapping(value = "/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
     }
 
     @Data
@@ -74,6 +119,16 @@ public class MemberApiController {
     @Data @AllArgsConstructor
     static class UpdateMemberResponse {
         private Long id;
+        private String name;
+    }
+
+    @Data @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data @AllArgsConstructor
+    static class MemberDto {
         private String name;
     }
 
