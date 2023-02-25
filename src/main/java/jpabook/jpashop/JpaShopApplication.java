99caf5d92fe5987a4be2260c99b84ad85b59763e
Revi1337 <david122123@gmail.com>
2023-02-25 18:21:54 +0900

    SimpleOrder v2 - Transform Entity to DTO

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -24,29 +24,29 @@ public class JpaShopApplication {
         return hibernate5Module;
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(MemberService memberService) {
-        return args -> {
-            Member member3 = new Member();
-            member3.setName("test1");
-            member3.setAddress(new Address("twest", "asdf", "2134"));
-            memberService.join(member3);
-
-            Member member4 = new Member();
-            member4.setName("test2");
-            member4.setAddress(new Address("wqwer213", "reqr", "asdf"));
-            memberService.join(member4);
-
-            Member member = new Member();
-            member.setName("member1");
-            member.setAddress(new Address("서울", "test", "11111"));
-            memberService.join(member);
-
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setAddress(new Address("부산", "qwer", "12322"));
-            memberService.join(member2);
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(MemberService memberService) {
+//        return args -> {
+//            Member member3 = new Member();
+//            member3.setName("test1");
+//            member3.setAddress(new Address("twest", "asdf", "2134"));
+//            memberService.join(member3);
+//
+//            Member member4 = new Member();
+//            member4.setName("test2");
+//            member4.setAddress(new Address("wqwer213", "reqr", "asdf"));
+//            memberService.join(member4);
+//
+//            Member member = new Member();
+//            member.setName("member1");
+//            member.setAddress(new Address("서울", "test", "11111"));
+//            memberService.join(member);
+//
+//            Member member2 = new Member();
+//            member2.setName("member2");
+//            member2.setAddress(new Address("부산", "qwer", "12322"));
+//            memberService.join(member2);
+//        };
+//    }
 
 }
--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -25,6 +25,14 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     *
+     * xToOne(ManyToOne, OneToOne) 관계 최적화
+     * Order
+     * Order -> Member
+     * Order -> Delivery
+     *
+     */
     @GetMapping(value = "/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -34,5 +42,38 @@ public class OrderSimpleApiController {
         }
         return all;
     }
-    
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping(value = "/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // ORDER 2개
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
+
+    }
+
 }
