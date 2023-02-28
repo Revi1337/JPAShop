12205793981869abc9fc3889376fd26fe657fd57
Revi1337 <david122123@gmail.com>
2023-03-01 00:40:24 +0900

    Order v3 - fetch join   (컬렉션 조회 최적화)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -43,7 +43,7 @@ public class OrderApiController {
     }
 
     /**
-     * 주문 조회 V2: 엔티티를 DTO 로 변환 --> DTO 변환한건 많이 아쉬운 방법 (연관관계 Entity 가 외부로 노출됨 ㅇㅇ) --> Entity 에 대한 의존을 완전히 끊어버려야 함.
+     * 주문 조회 V2: 엔티티를 DTO 로 변환 --> DTO 변환한건 좋지만 아쉬운 방법 (연관관계 Entity 가 외부로 노출됨 ㅇㅇ) --> Entity 에 대한 의존을 완전히 끊어버려야 함.
      * Entity 를  외부로 노출하지 말라는 것은 단순히 껍데기만 DTO 로 노출하라는 것이 아니라, 속에 있는 Entity 까지 노출하지말라는것임.
      * 속에 있는 Entity 가 무슨의미냐면, DTO 로 감깐 Entity 안에 존재하는 연관관계를 맺고있는 다른 Entity 를 말하는 것임.
      * V2 의 예에서는 Order 엔티티안의 OrderItem 엔티티도 DTO 로 변환시켜주어야 한다는 것임.
@@ -59,6 +59,28 @@ public class OrderApiController {
                 .map(OrderDto::new)
                 .collect(Collectors.toList());
         return collect;
+    }
+
+    /**
+     * 주문 조회 V3: 엔티티를 DTO 로 변환 - fetch join 최적화 --> fetch join 으로 V2 의 단점을 극복 --> 하지만 페이징 처리 불가
+     * findAllWithItem() 에서 `distinct` 를 사용이유는 1:N 조인이 있으므로 DB 의 row 가 증가됨(N 의 수가 맞춰짐). 그 결과 같은 order 엔티티의 조회수도 증가하게 됨. JPA 의 distinct 는 SQL 에 distinct 를 추가하고, 더해서
+     * 같은 엔티티가 조회되면 (pk 로 판단), 애플리케이션의 중복을 걸러줌. 결국은 order 가 컬렉션 페치 조인때문에 중복 조회되는 것을 막아주는 것임. --> 추가적으로 hibernate 6.0 부터는 distinct 를 쓰지 않아도 중복을 제거해줌.
+     *
+     * 단점으로는 페이징 불가능 --> 컬렉션(1:N) 에서 fetch join 이든 그냥 join 조인이든 join 쿼리가 나가면 페이징 처리가 불가함. 로그를 보면 (firstResult/maxResults specified with collection fetch; applying in memory) 가 뜸
+     *                         fetch 조인을 포함해서 1:N 에서 조인쿼리가 나가버리는순간 DB 의 row 가 뻥튀기 되면서, 순서의 기준을 잃어버리는 것임.
+     * 또다른 단점 --> 컬렉션(1:N) fetch join 은 1개만 사용 가능함. 컬렉션 둘 이상에 fetch join 을 사용하면 안됨. --> 데이터가 부정함하게 조회될 수 있음
+     */
+    @GetMapping(value = "/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        for (Order order : orders) {
+            System.out.println("order ref = " + order + " id=" + order.getId());
+        }
+
+        List<OrderDto> result = orders.stream()
+                .map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+        return result;
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -96,4 +96,21 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        // JPA 에서 distinct 는 두가지 기능을 함.
+        // 1. from 뒤의 첫번째 Entity (여기서는 Order) 가 중복인 경우, Entity 의 중복(pk 로 판단)을 제거해서 컬렉션을 담아줌.
+        // 2. DB 에 distinct 를 날림 --> 하지만 DB 에는 조인한 모든 컬럼이 한줄로 출력되기때문에 distinct 되는경우는 거의 없음.
+        // TODO 하지만, hibernate 6.0 부터는 알아서 distinct 를 넣지 않아도 from 절 뒤 첫번째로 오는 Entity 의 중복을 제거해줌. (알고만 있을것)
+        // TODO 개중요한것. 컬렉션(1:N) 에서 fetch join 이든 그냥 join 조인이든 join 쿼리가 나가면 페이징 처리가 불가함. 로그를 보면 (firstResult/maxResults specified with collection fetch; applying in memory) 가 뜸
+        // 이 뜻은 페이징처리인 firstResult/maxResults 가 설정되었지만, collection fetch 랑 같이 조인되었기 때문에 메모리상에서 페이징처리를 할것이라는 경고가 나오는것임. --> 데이터가 만약 10000개 면 이걸 메모리에서 페이징 --> out of memory
+        // fetch 조인을 포함해서 1:N 에서 조인쿼리가 나가버리는순간 DB 의 row 가 뻥튀기 되면서, 순서의 기준을 잃어버리는 것임.
+        // TODO 컬렉션(1:N) fetch join 은 1개만 사용 가능함. 컬렉션 둘 이상에 fetch join 을 사용하면 안됨. --> 데이터가 부정함하게 조회될 수 있음
+        return em.createQuery(
+                "select distinct o from Order o" + // hibernate 6.0 이상부터는 distinct 쓰지 않아도 됨.
+                " inner join fetch o.member m" +
+                " inner join fetch o.delivery d" +
+                " inner join fetch o.orderItems oi" +
+                " inner join fetch oi.item i" , Order.class
+        ).setFirstResult(0).setMaxResults(100).getResultList();
+    }
 }
